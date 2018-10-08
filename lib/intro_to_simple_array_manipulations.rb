ed45ee49575011a6c0c9e2f2ce2163261edca382
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-08 19:53:23 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,14 @@
-def usingpush(array,elm)
+def using_push(array,elm)
   array.push(elm)
   array
 end
 
-def usingpop(array)
+def using_pop(array)
   array.pop
 end
+
+def using_unshift(array,elm)
+  array.unshift(elm)
+end
+
+def 
